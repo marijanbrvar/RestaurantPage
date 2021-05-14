c12086c97599fb9cdf5ee408d57e79f02cd329e1
Marijan Brvar <marijan.brvar@gmail.com>
2021-05-14 18:18:09 +0200

    Add comonent to layout for dev purposes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
+import './ui/styles/reset.css';
 import './ui/styles/main.css';
-import Tooltip from './ui/tooltip';
 
-// create tooltip
-const tooltip = new Tooltip(document.querySelector('.tooltip'));
-tooltip.init();
+import heading from './ui/heading';
+import tabs from './ui/tabs';
+import home from './ui/home';
+import contact from './ui/contact';
+import delivery from './ui/delivery';
+
+tabs();
+delivery();
+home();
+contact();
+heading();
